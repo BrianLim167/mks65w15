257a955cf519cd1d750258f796339b28b3b6af4f
jenyu7 <jyu7@stuy.edu>
2017-12-09 09:38:11 -0500

    creation step done

--- semaphone.c
+++ semaphone.c
@@ -1,40 +1,49 @@
-#include 'head.h'
+#include "head.h"
+
+void print_error(int val){
+  if (val == -1){
+    printf("Error occurred: %s\n", strerror(errno));
+    exit(0);
+  }
+}
 
 int main(int argct, char** args){
-  int sem;
+  int sem, shm;
+  int fd;
   if(argct < 2){
     printf("Too few arguments.\n");
     exit(0);
   }
   else{
     //semaphore creation!
-    //if more than 3 arguments "./run -c <num" and strcmp() == 0 
-    if((argct >= 3) && (strcmp(args[1], "-c") == 0)){
-      sem = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
-      //if already exists, errno
-      if (sem == -1){
-	printf("The semaphore already exists.\n");
-      }
-      //else take inputted # and set val
-      else{
-	int input;
-	sscanf(args[2], "%d", &input);
-	int det = semctl(sem, 0, SETVAL, input);
-	if (det == -1){printf("Error occured: %s\n", strerror(errno));}
-	else{printf("Semaphore %d created with value %d.\n", sem, input);}
-      }
+    //if strcmp() == 0 
+    if(strcmp(args[1], "-c") == 0){
+      sem = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
+      print_error(sem);
+      union semun su;
+      su.val = 8;
+      int val = semctl(sem, 0, SETVAL, su);
+      print_error(val);
+      printf("Semaphore %d created with value %d.\n", sem, 8);
+      shm = shmget(SHM_KEY, 256, IPC_CREAT | 0644);
+      print_error(shm);
+      printf("Shared memory segment created.\n");
+      fd = open("story", O_CREAT | O_APPEND, 0644);
+      close(fd);
+      print_error(fd);
+      printf("Story created.\n");
     }
     //semaphore value procural!!
     //gets the semaphore val
     else if(strcmp(args[1], "-v") == 0){
-      sem = semget(KEY, 0, 0);
+      sem = semget(SEM_KEY, 0, 0);
       int val = semctl(sem, 0, GETVAL);
       if (val == -1){printf("Error occured: %s\n", strerror(errno));}
       else{printf("Value of semaphore: %d\n", val);}
     }
     //remove the semaphore :'(
     else if (strcmp(args[1], "-r") == 0){
-      sem = semget(KEY, 0, 0);
+      sem = semget(SEM_KEY, 0, 0);
       int val = semctl(sem, 0, IPC_RMID);
       if (val == -1){printf("Error occured: %s\n", strerror(errno));}
       else{printf("Removed semaphore: %d\n", val);}
