c4113d8b0370f31b730fc60f9cd1b09d2629a295
jenyu7 <jyu7@stuy.edu>
2017-12-09 11:01:40 -0500

    tryna figure this thing out

--- client.c
+++ client.c
@@ -9,5 +9,24 @@
 
 int main()
 {
+  int sem, shm, fd;
+  sem = semget(SEM_KEY, 1, 0);
+  if (sem == -1){
+    printf("semaphore does not exist.\n");
+    exit(0);
+  }
+  printf("waiting for opening...\n");
+  //semop(sem, sembuf, size_t nspos)
+  struct sembuf sbuf;
+  sbuf.sem_num = 0;
+  sbuf.sem_op = -1;
+  printf("%d\n",semctl(sem, 0, GETVAL));
+  sleep(1);
+  sbuf.sem_flg = IPC_NOWAIT;
+  semop(sem, &sbuf, 1);
+  printf("%d\n",semctl(sem, 0, GETVAL));
+  printf("done waiting!\n");
+  fd = open("story", O_RDWR | O_APPEND);
+  
   return 0;
 }
--- makefile
+++ makefile
@@ -1,5 +1,6 @@
-all: control.c
+all: control.c client.c
 	gcc control.c -o control
+	gcc client.c -o client
 clean:
 	rm -f *~ *.o control client
 wipe:
