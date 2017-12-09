4973aecf33c9f33bf6555bb49431f33b4a54f218
jenyu7 <jyu7@stuy.edu>
2017-12-09 16:07:42 -0500

    working

--- client.c
+++ client.c
@@ -20,13 +20,35 @@ int main()
   struct sembuf sbuf;
   sbuf.sem_num = 0;
   sbuf.sem_op = -1;
-  printf("%d\n",semctl(sem, 0, GETVAL));
-  sleep(1);
-  sbuf.sem_flg = IPC_NOWAIT;
+  sbuf.sem_flg = SEM_UNDO;
+  //printf("%d\n",semctl(sem, 0, GETVAL));
+  //sleep(1);
   semop(sem, &sbuf, 1);
-  printf("%d\n",semctl(sem, 0, GETVAL));
-  printf("done waiting!\n");
+  printf("accessed story!\n");
   fd = open("story", O_RDWR | O_APPEND);
-  
+  shm = shmget(SHM_KEY, 8, 0);
+  //get strlen of last addition
+  int *len = shmat(shm, 0, 0);
+  //seek -strlen bytes off the end of file
+  lseek(fd, -1* *len, SEEK_END);
+  //get the last addition to the story
+  char *latest = (char *)calloc(1, *len + 1);
+  read(fd, latest, *len);
+  printf("The latest update to the story:\n %s\n", latest);
+  free(latest);
+  //ask for an addition
+  printf("Add to the story: \n");
+  char new[2048];
+  fgets(new, 2048, stdin);
+  //update the shm
+  *len = strlen(new);
+  //write the input to the end of the story
+  lseek(fd, 0, SEEK_END);
+  write(fd, new, strlen(new));
+  //detach the shm
+  shmdt(len);
+  //release the semaphore
+  sbuf.sem_op = 1;
+  semop(sem, &sbuf, 1);
   return 0;
 }
